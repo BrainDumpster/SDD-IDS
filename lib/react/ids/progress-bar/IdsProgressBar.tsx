83a659eec7caa8cd1364c4602d5110471fdfeae5
CharlesDao <charles@daodesign.com>
2026-09-25 07:48:37 +0700

    [IDS] - add ressponsive helper text

--- lib/react/ids/progress-bar/IdsProgressBar.tsx
+++ lib/react/ids/progress-bar/IdsProgressBar.tsx
@@ -250,11 +250,16 @@ export function IdsProgressBar({
       )}
 
       {showHelperText && helperText ? (
-        <IdsHelper data-ids="ids-progress-bar-helper">
+        <IdsHelper
+          className={styles["ids-progress-bar-helper"]}
+          data-ids="ids-progress-bar-helper"
+        >
           {showHelperIcon ? (
             <IdsIcon shape={helperIconSlug!} variant="img" size={16} />
           ) : null}
-          <IdsHelperText>{helperText}</IdsHelperText>
+          <IdsHelperText className={styles["ids-progress-bar-helper-text"]}>
+            {helperText}
+          </IdsHelperText>
         </IdsHelper>
       ) : null}
     </div>
